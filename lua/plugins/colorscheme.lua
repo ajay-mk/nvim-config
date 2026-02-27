40a48c833905fddf8e4354eed02ba9a107251ab6
Ajay <ajaymk22@vt.edu>
2026-02-27 12:55:47 -0500

    feat: switch active colorscheme to darcula
    Replace tokyonight as the default theme with doums/darcula for a
    JetBrains-style look. Tokyonight moved to lazy-loaded option.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,13 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "doums/darcula",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "night",
-      transparent = false,
-      terminal_colors = true,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-      },
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
+    config = function()
+      vim.cmd.colorscheme("darcula")
     end,
   },
+  { "folke/tokyonight.nvim", lazy = true },
   { "catppuccin/nvim", name = "catppuccin", lazy = true },
   { "rebelot/kanagawa.nvim", lazy = true },
   { "rose-pine/neovim", name = "rose-pine", lazy = true },
