78b11884fb46218a990c466ac698844c30fe9aa1
Ajay <ajaymk22@vt.edu>
2026-07-15 17:54:16 -0400

    feat: configure nvim-dap with codelldb for C/C++ debugging

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -8,6 +8,7 @@ wk.setup({
 wk.add({
   { "<leader>b", group = "Buffer" },
   { "<leader>c", group = "Code" },
+  { "<leader>d", group = "Debug" },
   { "<leader>f", group = "Find" },
   { "<leader>g", group = "Git" },
   { "<leader>h", group = "Hunk" },
